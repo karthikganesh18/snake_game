4f31720becf29b342829b85c872f59e1aa6e760b
karthikganesh18 <142075105+karthikganesh18@users.noreply.github.com>
2023-08-15 15:55:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,49 +4,47 @@ from food import Food
 from scoreboard import Scoreboard
 import time
 
+# Set up the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My Snake Game")
+screen.title("Colorful Snake Game")
 screen.tracer(0)
 
+# Initialize game objects
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+# Set up key listeners
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+# Main game loop
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    #Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    # Detect collision with wall or tail.
+    if (
+        abs(snake.head.xcor()) > 280
+        or abs(snake.head.ycor()) > 280
+        or any(segment == snake.head for segment in snake.segments[1:])
+    ):
         game_is_on = False
         scoreboard.game_over()
 
-    #Detect collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
-
-
-
-
+# Exit the game when the user clicks
 screen.exitonclick()
+
