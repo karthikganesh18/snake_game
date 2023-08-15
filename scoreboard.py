b242a21158fb467c612e049f418f69cdc99f9b48
karthikganesh18 <142075105+karthikganesh18@users.noreply.github.com>
2023-08-15 15:56:00 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -15,11 +15,11 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Your Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+        self.write("GAME OVER! Play Again", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
